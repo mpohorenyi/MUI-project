f7a402cb634e865f844f905e9696e9abf24007df
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-02-01 21:08:05 +0200

    add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { AppBar, Toolbar } from '@mui/material';
+import { Mail, Notifications } from '@mui/icons-material';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { MUIIcon } from './MUIIcon';
+import { SearchBar } from './SearchBar';
 
 export const Navbar: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (): void => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="sticky">
-      <Toolbar>Navbar</Toolbar>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          MUI Project
+        </Typography>
+
+        <MUIIcon />
+
+        <SearchBar />
+
+        <Box display="flex" gap={1} alignItems="center">
+          <Box display={{ xs: 'none', sm: 'block' }}>
+            <IconButton size="large" color="inherit" sx={{ p: 1 }}>
+              <Badge badgeContent={4} color="error">
+                <Mail />
+              </Badge>
+            </IconButton>
+            <IconButton size="large" color="inherit" sx={{ p: 1 }}>
+              <Badge badgeContent={7} color="error">
+                <Notifications />
+              </Badge>
+            </IconButton>
+          </Box>
+          <IconButton
+            size="large"
+            color="inherit"
+            sx={{ p: 1 }}
+            onClick={handleClick}
+          >
+            <Avatar sx={{ width: 32, height: 32 }} />
+          </IconButton>
+        </Box>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={isOpen}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
     </AppBar>
   );
 };
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,51 @@
+import { InputBase, alpha, styled } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import React from 'react';
+
+export const SearchBar: React.FC = () => {
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '40%',
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </Search>
+  );
+};
