9b7d25a3f7e826c42eb168e3962c56a1fc8dbf9a
Mykhailo Pogorenyi <mpohorenyi@gmail.com>
2024-02-02 21:40:42 +0200

    add Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
 
-import { Box } from '@mui/material';
+import {
+  AccountBox,
+  Article,
+  DarkMode,
+  Group,
+  Home,
+  Person,
+  Settings,
+  Storefront,
+} from '@mui/icons-material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
+
+const listCategory = [
+  { title: 'Home', icon: <Home /> },
+  { title: 'Pages', icon: <Article /> },
+  { title: 'Groups', icon: <Group /> },
+  { title: 'Marketplace', icon: <Storefront /> },
+  { title: 'Friends', icon: <Person /> },
+  { title: 'Settings', icon: <Settings /> },
+  { title: 'Profile', icon: <AccountBox /> },
+];
 
 export const Sidebar: React.FC = () => {
   return (
-    <Box
-      bgcolor="red"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: 'none', sm: 'block' } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        {listCategory.map((item) => (
+          <ListItemButton key={item.title}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemText primary={item.title} />
+          </ListItemButton>
+        ))}
+
+        <ListItem>
+          <ListItemIcon>
+            <DarkMode />
+          </ListItemIcon>
+          <Switch />
+        </ListItem>
+      </List>
     </Box>
   );
 };
